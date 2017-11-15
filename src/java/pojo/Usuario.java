3c0feb13038ca69189942af6253f3fcba309d870
iEnriqueR <32380322+iEnriqueR@users.noreply.github.com>
2017-11-15 10:47:05 -0600

    Primer commit

---
+++ src/java/pojo/Usuario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pojo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author Enrique
+ */
+@Entity
+@Table(name="usuario", catalog="mydb")
+public class Usuario {
+    
+}
